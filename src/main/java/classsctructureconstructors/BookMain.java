c84f3d5803f870c379bbd2c13083d56052172ac9
Edina <terjek.edina@gmail.com>
2020-11-01 20:23:52 +0100

    Könyv osztály

---
+++ src/main/java/classsctructureconstructors/BookMain.java
@@ -0,0 +1,12 @@
+package classsctructureconstructors;
+
+public class BookMain {
+    public static void main(String[] args) {
+        Book book = new Book("L. Ny. Tolsztoj", "Háború és béke");
+        book.register("035648");
+
+        System.out.println(book.getAuthorAndTitle());
+        System.out.println(book.getRegNumber());
+
+    }
+}
