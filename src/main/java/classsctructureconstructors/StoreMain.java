6ac9837aeef384afc454da1b3963c4e32e7d4f27
Edina <terjek.edina@gmail.com>
2020-11-01 21:43:54 +0100

    Raktár (alma és körte)

---
+++ src/main/java/classsctructureconstructors/StoreMain.java
@@ -0,0 +1,32 @@
+package classsctructureconstructors;
+
+public class StoreMain {
+    public static void main(String[] args) {
+        Store store = new Store("Alma");
+
+        store.store(20);
+        store.dispatch(4);
+
+        System.out.println(store.getProduct() + " " + store.store() + " kg");
+
+        store.store(30);
+        store.dispatch(2);
+
+        System.out.println(store.getProduct() + " " + store.store() + " kg");
+
+
+
+        Store storeTwo = new Store("Körte");
+
+        storeTwo.store(50);
+        storeTwo.dispatch(8);
+
+        System.out.println(storeTwo.getProduct() + " " + storeTwo.store() + " kg");
+
+        storeTwo.store(10);
+        storeTwo.dispatch(2);
+
+        System.out.println(storeTwo.getProduct() + " " + storeTwo.store() + " kg");
+
+    }
+}
