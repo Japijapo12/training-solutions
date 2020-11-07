daebd4bf6f149392843c15686e8627eb2288f42b
Edina <terjek.edina@gmail.com>
2020-11-07 13:41:36 +0100

    greetings

---
+++ src/main/java/packages/greatings/Greeter.java
@@ -0,0 +1,7 @@
+package packages.greatings;
+
+public class Greeter {
+    public void sayHello() {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/packages/main/MainProgram.java
@@ -0,0 +1,9 @@
+package packages.main;
+
+import packages.greatings.Greeter;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        new Greeter().sayHello();
+    }
+}
