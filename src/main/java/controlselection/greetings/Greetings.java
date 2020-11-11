a7be9e48976f105d03f83982d4687731e0f9c5b4
Edina <terjek.edina@gmail.com>
2020-11-11 21:24:21 +0100

    Greetings

---
+++ src/main/java/controlselection/greetings/Greetings.java
@@ -0,0 +1,27 @@
+package controlselection.greetings;
+
+public class Greetings {
+
+    int hour;
+    int minutes;
+    public static final int MINUTES = 60;
+
+    int inMinutes = hour * MINUTES + minutes;
+
+    public String greet(int hour, int minutes) {
+        int inMinutes = hour * MINUTES + minutes;
+        if (inMinutes <= 5  * MINUTES || inMinutes > 20 * MINUTES) {
+            return "jó éjt";
+        }
+        else if (inMinutes > 5 * MINUTES && inMinutes <= 9 * MINUTES) {
+            return "jó reggelt";
+        }
+        else if (inMinutes > 9 * MINUTES && inMinutes <= 18 * MINUTES + 30) {
+            return "jó napot";
+        }
+        else {
+            return "jó estét";
+        }
+    }
+
+}
