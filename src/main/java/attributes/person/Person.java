efae31a78891fe92cec2740d96efb8401a2ae85e
Edina <terjek.edina@gmail.com>
2020-11-11 21:12:10 +0100

    person, moveTo

--- src/main/java/attributes/person/Person.java
+++ src/main/java/attributes/person/Person.java
@@ -2,8 +2,9 @@ package attributes.person;
 
 public class Person {
 
-    String name;
-    String identificationCard;
+    private String name;
+    private String identificationCard;
+    private Address address;
 
     public Person (String name, String identificationCard) {
         this.name = name;
@@ -25,9 +26,25 @@ public class Person {
 
     }
 
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", identificationCard='" + identificationCard + '\'' +
+                '}';
+    }
+
     public void correctData (String name, String identificationCard) {
         this.name = name;
         this.identificationCard = identificationCard;
+        }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void moveTo(Address address) {
+        this.address = address;
         }
 
 }
--- src/main/java/attributes/person/PersonMain.java
+++ src/main/java/attributes/person/PersonMain.java
@@ -1,5 +1,7 @@
 package attributes.person;
 
+import java.util.Scanner;
+
 public class PersonMain {
     public static void main(String[] args) {
 
@@ -10,5 +12,31 @@ public class PersonMain {
         person.correctData("Jack Doe", "PU-4234220");
 
         System.out.println(person.personToString());
+
+
+        System.out.println("Country:");
+        Scanner scanner = new Scanner(System.in);
+        String country = scanner.nextLine();
+
+        System.out.println("City:");
+        String city = scanner.nextLine();
+
+        System.out.println("streetAndNumber:");
+        String streetAndNumber = scanner.nextLine();
+
+        System.out.println("zipCode:");
+        String zipCode = scanner.nextLine();
+
+        Address address = new Address(country, city, streetAndNumber, zipCode);
+        System.out.println(address);
+
+        address.correctData("Spanyolo.", "Barcelona", "jljl 3", "32532452");
+
+        System.out.println(address.addressToString() );
+
+
+
+
     }
+
 }
