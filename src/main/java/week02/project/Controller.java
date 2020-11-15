f2647794575fb6e71d2300caa20adc695274c2d9
Edina <terjek.edina@gmail.com>
2020-11-15 19:24:53 +0100

    projektmunka printNamesReverse ? printEventNames ?

---
+++ src/main/java/week02/project/Controller.java
@@ -0,0 +1,103 @@
+package week02.project;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Controller {
+
+    private final Office office = new Office();
+
+    public void readOffice() {
+        System.out.println("Adja meg a tárgyalók számát!");
+        Scanner scanner = new Scanner(System.in);
+
+        int meetingRoomCount = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < meetingRoomCount; i++) {
+            System.out.println("Adja meg a tárgyaló nevét!");
+            String name = scanner.nextLine();
+            System.out.println("Adja meg a tárgyaló hosszúságát!");
+            int length = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Adja meg a tárgyaló szélességét!");
+            int width = scanner.nextInt();
+            scanner.nextLine();
+
+            MeetingRoom meetingRoom = new MeetingRoom(name, length, width);
+            office.addMeetingRoom(meetingRoom);
+
+
+        }
+        System.out.println(office.getMeetingRooms());
+
+
+    }
+
+    public void printMenu() {
+        System.out.println("1. Tárgyalók sorrendben\n" +
+                "2. Tárgyalók visszafele sorrendben\n" +
+                "3. Minden második tárgyaló\n" +
+                "4. Területek\n" +
+                "5. Keresés pontos név alapján\n" +
+                "6. Keresés névtöredék alapján\n" +
+                "7. Keresés terület alapján");
+
+    }
+
+    public void runMenu() {
+        System.out.println("Melyik menüpontot választja?");
+        Scanner scanner = new Scanner(System.in);
+        int menuItem = scanner.nextInt();
+        scanner.nextLine();
+
+        if (menuItem == 1) {
+            office.printNames();
+        }
+
+       /* if (menuItem == 2) {
+            office.printNamesReverse();
+        }*/
+
+      /*  if (menuItem == 3) {
+            office.printEventNames();
+        }*/
+
+        if (menuItem == 4) {
+            office.printAreas();
+        }
+
+        if (menuItem == 5) {
+            System.out.println("Adja meg a keresett tárgyaló pontos nevét:");
+            String name = scanner.nextLine();
+
+            office.printMeetingRoomsWithNames(name);
+        }
+
+        if (menuItem == 6) {
+            System.out.println("Adja meg a név részletét:");
+            String namefragment = scanner.nextLine();
+
+            office.printMeetingRoomsContains(namefragment);
+        }
+
+        if (menuItem == 7) {
+            System.out.println("Mekkora területnél legyen nagyobb?");
+            int area = scanner.nextInt();
+            scanner.nextLine();
+
+            office.printAreasLargerThan(area);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.readOffice();
+        controller.printMenu();
+        controller.runMenu();
+    }
+
+
+}
---
+++ src/main/java/week02/project/Office.java
@@ -0,0 +1,77 @@
+package week02.project;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Office {
+
+    public List<MeetingRoom> meetingRooms = new ArrayList<>();
+
+    public void addMeetingRoom(MeetingRoom meetingRoom) {
+        meetingRooms.add(meetingRoom);
+
+    }
+
+    public void printNames() {
+        for (MeetingRoom meetingRoom : meetingRooms) {
+            System.out.println(meetingRoom.getName());
+        }
+
+    }
+
+   public void printNamesReverse() {
+
+   }
+
+    public void printEventNames() {
+
+    }
+
+
+    public void printAreas() {
+        for (MeetingRoom meetingRoom : meetingRooms) {
+            System.out.println(meetingRoom.getName() + " (" + meetingRoom.getWidth() + " * " + meetingRoom.getLength() + ") területe: " + meetingRoom.getArea());
+        }
+    }
+
+    public void printMeetingRoomsWithNames(String name) {
+        for (MeetingRoom meetingRoom : meetingRooms) {
+            if (meetingRoom.getName().contains(name)) {
+                System.out.println(meetingRoom.getName() + " (" + meetingRoom.getWidth() + " * " + meetingRoom.getLength() + ") területe: " + meetingRoom.getArea());
+            }
+
+        }
+
+    }
+
+
+    public void printMeetingRoomsContains(String namefragment) {
+        for (MeetingRoom meetingRoom : meetingRooms) {
+            if (meetingRoom.getName().contains(namefragment)) {
+                System.out.println(meetingRoom.getName() + " (" + meetingRoom.getWidth() + " * " + meetingRoom.getLength() + ") területe: " + meetingRoom.getArea());
+            }
+
+        }
+
+    }
+
+
+    public void printAreasLargerThan(int area) {
+        for (MeetingRoom meetingRoom : meetingRooms) {
+            if (meetingRoom.getArea() > area) {
+                System.out.println(meetingRoom.getName() + " (" + meetingRoom.getWidth() + " * " + meetingRoom.getLength() + ") területe: " + meetingRoom.getArea());
+            }
+        }
+
+    }
+
+    public List<MeetingRoom> getMeetingRooms() {
+        return meetingRooms;
+    }
+
+    public void setMeetingRooms(List<MeetingRoom> meetingRooms) {
+        this.meetingRooms = meetingRooms;
+    }
+}
