3a97057cd57f921953d9bcb39d5659090311a50f
Edina <terjek.edina@gmail.com>
2020-11-16 18:24:10 +0100

    projektmunka - MeetingRoom

---
+++ src/main/java/week02/project/MeetingRoom.java
@@ -0,0 +1,43 @@
+package week02.project;
+
+public class MeetingRoom {
+    private String name;
+
+    private int length;
+
+    private int width;
+
+    public MeetingRoom(String name, int length, int width) {
+        this.name = name;
+        this.length = length;
+        this.width = width;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getArea() {
+        return length * width;
+        }
+
+
+    @Override
+    public String toString() {
+        return "MeetingRoom{" +
+                "name='" + name + '\'' +
+                ", length=" + length +
+                ", width=" + width +
+                '}';
+    }
+
+
+}
