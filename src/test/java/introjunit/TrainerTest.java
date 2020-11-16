c461900de013b054b6a10c8c1337096d26118908
Edina <terjek.edina@gmail.com>
2020-11-16 21:06:50 +0100

    ok.

--- src/test/java/introjunit/TrainerTest.java
+++ src/test/java/introjunit/TrainerTest.java
@@ -1,8 +1,8 @@
 package introjunit;
 
-        import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
-        import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TrainerTest {
 
