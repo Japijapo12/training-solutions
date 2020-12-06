fce6b1f3372a427814a735a491551b2cbd42bed1
Edina <terjek.edina@gmail.com>
2020-12-06 13:08:42 +0100

    javítva

--- src/main/java/classstructureio/Calculator.java
+++ src/main/java/classstructureio/Calculator.java
@@ -6,14 +6,13 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Kérlek, adj meg egy számot!");
         Scanner scanner = new Scanner(System.in);
-        int NumberOne = scanner.nextInt();
+        int numberOne = scanner.nextInt();
 
         System.out.println("Adj meg egy másik számot is!");
-        Scanner scanner2 = new Scanner(System.in);
-        int NumberTwo = scanner2.nextInt();
+        int numberTwo = scanner.nextInt();
 
-        System.out.println(NumberOne + " + " + NumberTwo);
-        System.out.println(NumberOne + NumberTwo);
+        System.out.println(numberOne + " + " + numberTwo);
+        System.out.println(numberOne + numberTwo);
 
     }
 
