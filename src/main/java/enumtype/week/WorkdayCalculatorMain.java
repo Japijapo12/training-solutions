df4fe24252d01910a5ea67b8a335f426412df2ca
Edina <terjek.edina@gmail.com>
2020-12-11 20:41:33 +0100

    enum - Hét napjai (teszt)

---
+++ src/main/java/enumtype/week/WorkdayCalculatorMain.java
@@ -0,0 +1,12 @@
+package enumtype.week;
+
+public class WorkdayCalculatorMain {
+    public static void main(String[] args) {
+
+      WorkdayCalculator workdayCalculator = new WorkdayCalculator();
+
+
+
+        System.out.println(workdayCalculator.dayTypes(Day.THURSDAY, 7));
+    }
+}
