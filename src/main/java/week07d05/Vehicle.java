0f288e78b18a0f54074d353a33661070d3b4e4af
Edina <terjek.edina@gmail.com>
2020-12-11 20:01:00 +0100

    órai feladat - öröklődés

---
+++ src/main/java/week07d05/Vehicle.java
@@ -0,0 +1,24 @@
+package week07d05;
+
+public class Vehicle {
+
+    private int numberOfGears;  //váltók száma
+    private TransmissionType transmissionType;
+
+
+    public Vehicle(int numberOfGears, TransmissionType transmissionType) {
+        this.numberOfGears = numberOfGears;
+        this.transmissionType = transmissionType;
+    }
+
+    public final int getNumberOfGears() {
+        return numberOfGears;
+    }
+
+
+    public final TransmissionType getTransmissionType() {
+        return getTransmissionType();
+
+    }
+
+}
