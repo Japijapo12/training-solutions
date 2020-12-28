bf2a6b64804af3ad467d53cea7bc6a004b009c4e
Edina <terjek.edina@gmail.com>
2020-12-28 22:18:40 +0100

    páros szám

---
+++ src/main/java/operators/OperatorsMain.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class OperatorsMain {
+
+    public static void main(String[] args) {
+
+
+        Operators operators = new Operators();
+
+        System.out.println(operators.isEven(23));
+        System.out.println(operators.isEven(24));
+    }
+}
+
