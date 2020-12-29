349e6d44914ed3505e984781dbe9288725f8b14f
Edina <terjek.edina@gmail.com>
2020-12-29 21:13:58 +0100

    Létező interface használata (elemet kiveszi a listából)

---
+++ src/main/java/interfaces/SimpleThread.java
@@ -0,0 +1,29 @@
+package interfaces;
+
+import java.util.List;
+
+public class SimpleThread implements Runnable {
+
+    private List<String> tasks;
+
+    public List<String> getTasks() {
+        return tasks;
+    }
+
+    public SimpleThread(List<String> tasks) {
+        this.tasks = tasks;
+    }
+
+    private boolean nextStep() {
+        if (tasks !=null && !tasks.isEmpty()) {
+            tasks.remove(tasks.size()-1);
+        }
+    }
+
+    @Override
+    public void run() {
+        while(!tasks.isEmpty()) {
+            nextStep();
+        }
+    }
+}
