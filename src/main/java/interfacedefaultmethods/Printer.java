fd5cb3ab2194c7e9bb76ce39168b88363dbb1cd9
Edina <terjek.edina@gmail.com>
2020-12-30 22:07:30 +0100

    Printer

---
+++ src/main/java/interfacedefaultmethods/Printer.java
@@ -0,0 +1,17 @@
+package interfacedefaultmethods;
+
+public class Printer {
+
+    public String print(Printable printable) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < printable.getLength(); i++) {
+
+            if (!printable.BLACK.equals(printable.getColor(i))) {
+                stringBuilder.append("[" + (printable.getColor(i)) + "]");
+            }
+            stringBuilder.append(printable.getPage(i)).append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}
