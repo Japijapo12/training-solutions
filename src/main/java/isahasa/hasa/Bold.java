cae2c0bd9adfc88e33229240994e5b12fd472b4b
Edina <terjek.edina@gmail.com>
2021-01-02 19:13:31 +0100

    Html decorator - hasa

---
+++ src/main/java/isahasa/hasa/Bold.java
@@ -0,0 +1,16 @@
+package isahasa.hasa;
+
+import isahasa.TextSource;
+
+public class Bold extends TextDecorator {
+
+    public Bold(TextSource textSource) {
+        super(textSource);
+    }
+
+    @Override
+    public String getPlainText() {
+        return "<b>" + getPlainText() + "</b>";
+    }
+
+}
