c2bd7c0b5a6957f8ef67e6b5c5151ac2a1b61739
Edina <terjek.edina@gmail.com>
2021-01-06 19:42:58 +0100

    ArraySelector

---
+++ src/main/java/exam02/ArraySelector.java
@@ -0,0 +1,24 @@
+package exam02;
+
+public class ArraySelector {
+
+
+    public String selectEvens(int[] events) {
+        String str = "";
+        if ( events == null) {
+            return "";
+        }
+
+        for ( int i = 0; i < events.length; i++) {
+
+
+            if (i % 2 == 0) {
+              str = (events[i] + ", ").toString();
+            }
+        }
+        return str;
+    }
+
+
+
+}
