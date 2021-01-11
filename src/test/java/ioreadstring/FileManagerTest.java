d2f284243ce6991895efab21de1055645f962f08
Edina <terjek.edina@gmail.com>
2021-01-11 23:33:32 +0100

    Fájlkezelés - Humans teszt

---
+++ src/test/java/ioreadstring/FileManagerTest.java
@@ -0,0 +1,27 @@
+package ioreadstring;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileManagerTest {
+
+    FileManager fileManager = new FileManager("human.txt");
+
+
+   @Test
+   public void createClassTest() {
+       assertEquals("human.txt", fileManager.getFile().getFileName().toString());
+    }
+
+
+
+    @Test
+    void readFromFileTest() {
+        fileManager.readFromFile();
+        assertEquals(4, fileManager.getHumans().size());
+        assertEquals("Kovács", fileManager.getHumans().get(0).getFirstName());
+    }
+
+
+}
