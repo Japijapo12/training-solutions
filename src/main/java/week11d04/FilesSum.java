67ef0e1b30b7476e1ecfa627edf3f27097732225
Edina <terjek.edina@gmail.com>
2021-01-15 00:15:20 +0100

    Filesum - mi a hiba?

---
+++ src/main/java/week11d04/FilesSum.java
@@ -0,0 +1,47 @@
+package week11d04;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FilesSum {
+
+    private Path file;
+
+    public int readFile(String filename) {
+        //Path path = Path.of(filename);
+        int number;
+        try {
+            String fileContent = Files.readString(Path.of(filename));
+            number = Integer.parseInt(fileContent);    // a beolvasott txt fájlokban egy-egy szám van
+
+        } catch (IOException | NumberFormatException e) {
+            throw new IllegalArgumentException("Can not read the file", e);
+        }
+        return number;
+    }
+
+
+    public int sumNumbers() {
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            String filename = "number" + (i < 10 ? "0" + i : i) + "txt";
+             //String filename = String.format("number%02d.txt", i);
+
+            if (Files.isRegularFile(Path.of(filename))) {
+
+                int number = readFile(filename);
+                sum += number;
+            }
+
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FilesSum().sumNumbers());
+        System.out.println(new FilesSum().readFile("number20.txt"));
+
+    }
+
+}
