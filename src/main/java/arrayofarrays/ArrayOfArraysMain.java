2c2418606e32b3b5a39868847282944072942b3e
Edina <terjek.edina@gmail.com>
2021-01-21 20:06:48 +0100

    Tömbök tömbje

---
+++ src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -0,0 +1,73 @@
+package arrayofarrays;
+
+public class ArrayOfArraysMain {
+
+    public void printArrayOfArrays(int[][] array) {
+        for (int[] i : array) {
+            for (int j : i) {
+                if (j < 10) {
+                    System.out.print("  ");
+                } else if (j < 100) {
+                    System.out.print(" ");
+                }
+                System.out.print(j);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public int[][] multiplicationTable(int size) {
+        int[][] multiplicationTable = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                multiplicationTable[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return multiplicationTable;
+    }
+
+
+
+    //Háromszögmátrix
+
+    public int[][] triangularMatrix(int size) {
+        int[][] triangularMatrix = new int[size][];
+        for (int i = 0; i < triangularMatrix.length; i++) {
+            triangularMatrix[i] = new int[i + 1];
+            for (int j = 0; j < i + 1; j++) {
+                triangularMatrix[i][j] = i;
+            }
+        }
+        return triangularMatrix;
+    }
+
+
+    //ahány nap a hónapban, olyan hosszú egy tömb (12 tömb van)
+
+    public int[][] getValues() {
+        int[][] values = new int[12][];
+        int[] numberOfDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for (int i = 0; i < numberOfDays.length; i++) {
+            values[i] = new int[numberOfDays[i]];
+        }
+        return values;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        ArrayOfArraysMain arrayOfArraysMain = new ArrayOfArraysMain();
+
+        System.out.println("Multiplication table");
+        arrayOfArraysMain.printArrayOfArrays(arrayOfArraysMain.multiplicationTable(10));
+
+
+        System.out.println("Triangular matrix");
+        arrayOfArraysMain.printArrayOfArrays(arrayOfArraysMain.triangularMatrix(5));
+
+        System.out.println("Values per days");
+        arrayOfArraysMain.printArrayOfArrays(arrayOfArraysMain.getValues());
+    }
+}
