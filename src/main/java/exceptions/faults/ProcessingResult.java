d164a1b95c9346faef2d0a5937d485bab653f4d5
Edina <terjek.edina@gmail.com>
2021-01-23 22:05:41 +0100

    Bemenő adatok ellenőrzése

--- src/main/java/exceptions/faults/ProcessingResult.java
+++ src/main/java/exceptions/faults/ProcessingResult.java
@@ -20,3 +20,12 @@ public enum ProcessingResult {
 
 
 }
+
+
+//Hibakódok:
+//• 2: WORDCOUNT_ERROR, azaz a sor nem bontható 3 db , karakterrel elválaszható
+//részre.
+//• 4: VALUE_ERROR, azaz a második rész nem double szám.
+//• 8: DATE_ERROR, azaz a harmadik rész nem yyyy.MM.dd. alakú dátum
+//• 12: VALUE_AND_DATE_ERROR: azaz egyszerre van VALUE_ERROR és DATE_ERROR is.
+
