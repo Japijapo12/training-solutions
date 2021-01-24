5c75248322307da35ac11f7afd27b845832a882f
Edina <terjek.edina@gmail.com>
2021-01-24 20:26:04 +0100

    Hossszú szavak

--- src/main/java/iostringwriter/Longwords.java
+++ src/main/java/iostringwriter/Longwords.java
@@ -2,25 +2,23 @@ package iostringwriter;
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.List;
 
 public class Longwords {
 
-    public void writeLongWords(Writer writer, List<String> words) {
 
-        StringWriter stringWriter = new StringWriter();
-        try (stringWriter) {
-            for (String word : words) {
-                writer.write(word);
-                writer.write(": ");
-                writer.write(word.length());
+        public void writeLongWords(Writer writer, List<String> words){
+
+            PrintWriter printWriter = new PrintWriter(writer);
+            for(String s : words){
+                printWriter.print(s);
+                printWriter.print(":");
+                printWriter.println(s.length());
             }
-        } catch (IOException e) {
-            throw new IllegalStateException("Can not write  file", e);
         }
-    }
 
 
     public String writeWithStringWriter(List<String> words) {
