f9e63b4422f8ee13313afed015d80892e64583c4
Edina <terjek.edina@gmail.com>
2021-01-24 11:17:13 +0100

    Hőmérsékleti statisztika - feladatleírással

--- src/main/java/ioreadwritebytes/Temperatures.java
+++ src/main/java/ioreadwritebytes/Temperatures.java
@@ -28,3 +28,14 @@ public class Temperatures {
         return average / 30;
     }
 }
+
+//Hőmérsékleti statisztika
+//Az előző 365 nap hőmérsékleti adatait fájlban tároljuk. A Temperatures osztály
+//attribútumában ezek találhatók egy byte[]-ben. Az osztály konstruktorban kapja meg a
+//hőmérsékleti adatokat. A getYearAverage() metódusa a teljes év átlaghőmérsékletét
+//adja vissza, a getMonthAverage() az utolsó 30 napét. Készíts egy TemperatureReader
+//osztályt, mely egyetlen readTemperatures() metódusa bináris fájlból beolvassa az
+//eltárolt hőmérsékleteket, és egy Temperatures példánnyal tér vissza! Ugyanennek
+//mintájára készíts egy TemperaturesWriter osztályt, amelynek a writeTemperatures()
+//metódusa a paraméterként kapott Temperatures példányból bináris fájlba írja a
+//hőmérsékleti adatokat!
