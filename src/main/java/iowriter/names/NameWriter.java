1139cde381316ae6bbd96bde2baab15ec8b3f0dd
Edina <terjek.edina@gmail.com>
2021-01-24 11:37:42 +0100

    Nevek - szöveg írása fájlba BufferedWriterrel

---
+++ src/main/java/iowriter/names/NameWriter.java
@@ -0,0 +1,32 @@
+package iowriter.names;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public class NameWriter {
+
+    private Path file;
+
+    public NameWriter(Path file) {
+        this.file = file;
+    }
+
+    public void addAndWrite(String name) {
+        try(BufferedWriter writer = Files.newBufferedWriter(file, StandardOpenOption.APPEND)) {
+            writer.write(name + "\n");
+        }
+        catch (IOException e) {
+            throw new IllegalStateException("Can not write file", e);
+        }
+
+    }
+}
+
+//Neveket fogunk eltárolni fájlban és listában egyaránt. A
+//NameWriter osztály konstruktorban megkapja az írni kívánt fájlt. Az addAndWrite()
+//metódus egy nevet vár paraméterül, amelyet hozzáfűzi a fájlhoz. A hozzáfűzéshez
+//használd a korábban megismert StandardOpenOption.Append paramétert a
+//newBufferedWriter() metódusban.
