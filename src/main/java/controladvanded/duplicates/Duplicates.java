1ac0497bd4a2a56a1ab7d460768ad7faf5f7fcfc
Edina <terjek.edina@gmail.com>
2021-01-29 23:54:42 +0100

    findDuplicates() metódus listából duplán előforduló elemek szűrése

---
+++ src/main/java/controladvanded/duplicates/Duplicates.java
@@ -0,0 +1,27 @@
+package controladvanded.duplicates;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Duplicates {
+
+    public List<Integer> find(List<Integer> elements) {
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < elements.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (elements.get(i).equals(elements.get(j))) {
+                    result.add(elements.get(i));
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
+
+
+
+
+//Szűrd ki egy List<Integer> listában a többször szereplő elemeket, és add vissza.
