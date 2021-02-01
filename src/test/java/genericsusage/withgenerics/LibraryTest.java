d877b391c9131d67accb2e9ecbba3f1e221691cc
Edina <terjek.edina@gmail.com>
2021-02-01 21:18:26 +0100

    LibraryTest withgenerics

---
+++ src/test/java/genericsusage/withgenerics/LibraryTest.java
@@ -0,0 +1,30 @@
+package genericsusage.withgenerics;
+
+import genericsusage.Book;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class LibraryTest {
+
+    @Test
+    public void shouldThrowExceptionIfArgumentIsNull() {
+        assertThrows(NullPointerException.class, () -> new Library().getFirstBook(null));
+    }
+
+    @Test
+    public void shouldThrowExceptionIfArgumentIsEmptyList() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> new Library().getFirstBook(Collections.emptyList()));
+        assertEquals("Argument should not be empty!", ex.getMessage());
+    }
+
+    @Test
+    public void shouldReturnFirst() {
+        assertEquals("Egri Csillagok", new Library().getFirstBook(Arrays.asList(new Book("Gárdonyi Géza", "Egri Csillagok"))).getTitle());
+    }
+
+}
