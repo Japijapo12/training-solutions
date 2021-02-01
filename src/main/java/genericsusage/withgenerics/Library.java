5e75c9908b0b132310f9d3fcf4f062fbe29299fd
Edina <terjek.edina@gmail.com>
2021-02-01 21:19:24 +0100

    Library withgenerics

---
+++ src/main/java/genericsusage/withgenerics/Library.java
@@ -0,0 +1,24 @@
+package genericsusage.withgenerics;
+
+import genericsusage.Book;
+
+import java.util.List;
+
+public class Library {
+
+
+    public Book getFirstBook(List<Book> books) {
+
+        if ( books == null) {
+            throw new NullPointerException("This argument should not be null");
+        }
+
+        if (books.isEmpty()) {
+            throw new IllegalArgumentException("Argument should not be empty!");
+        }
+
+        return books.get(0);
+
+
+    }
+}
