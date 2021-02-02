2cf3f4e5d2f214a312cdfb1f4dc29efb3967c7a5
Edina <terjek.edina@gmail.com>
2021-02-02 19:35:30 +0100

    foreach -et használva

--- src/main/java/week11d01/DivisorFinder.java
+++ src/main/java/week11d01/DivisorFinder.java
@@ -2,18 +2,43 @@ package week11d01;
 
 public class DivisorFinder {
 
-   public int findDivisors(int n) {
+    public int findDivisors(int n) {
+            /*  int result = 0;
+                String number = Integer.toString(n);
+                for (int i = 0; i < number.length(); i++) {
+                if ( n % Integer.parseInt(String.valueOf(number.charAt(i))) == 0 ) {
+                    result ++;
+                  }
+                }
+             return result;
+          */
+
+        /*
         int result = 0;
         String number = Integer.toString(n);
-        for (int i =0; i<number.length(); i++) {
-          if ( n % Integer.parseInt(String.valueOf(number.charAt(i))) == 0 ) {
-              result ++;
-          }
+        for (int i = 0; i < number.length(); i++) {
+            int digit = Integer.parseInt(Character.toString(number.charAt(i)));
+            if (n % digit == 0) {
+                result++;
+            }
+        }
+        return result;
+        */
+
+        // for ciklus helyett foreach:
+
+        int result = 0;
+        String number = Integer.toString(n);
+
+        for (char c : number.toCharArray()) {
+            int digit = Integer.parseInt(Character.toString(c));
+            if (n % digit == 0) {
+                result ++;
+            }
         }
         return result;
     }
 }
-
 
 
 //Készítsünk egy DivisorFinder nevű osztályt, melynek van egy int findDivisors(int n) metódusa.
