4ef7715b76bf46312f0ff8e5c166d19e9a27f1df
Edina <terjek.edina@gmail.com>
2021-02-02 19:36:20 +0100

    format, %d alkalmazása

---
+++ src/main/java/week11d01/Strings.java
@@ -0,0 +1,10 @@
+package week11d01;
+
+public class Strings {
+
+    public static void main(String[] args) {
+
+        String value = String.format("Ez egy szám: °%d %s", 10,"John Doe");
+        System.out.println(value);
+    }
+}
