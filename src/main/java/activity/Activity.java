a11b39fbe6dc67784fa4c4c088673477f8da18a4
Edina <terjek.edina@gmail.com>
2021-02-03 22:29:54 +0100

    Activity project

--- src/main/java/activity/Activity.java
+++ src/main/java/activity/Activity.java
@@ -5,4 +5,8 @@ public interface Activity {
     double getDistance();
 
     ActivityType getType();
+
+    default boolean hasTrack() {   // csak akkor kell felülírni, ha true
+        return false;
+    }
 }
