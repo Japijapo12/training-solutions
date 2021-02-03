7dcf2e8c0f8b3f85a8f4a3f36ce629683b886fb6
Edina <terjek.edina@gmail.com>
2021-02-03 18:54:57 +0100

    Activity project segédmetódusokkal

--- src/main/java/activity/Track.java
+++ src/main/java/activity/Track.java
@@ -46,15 +46,56 @@ public class Track {
     }
 
 
+    public double getRectangleArea() {
+        Coordinate min = findMinimumCoordinate();
+        Coordinate max = findMaximumCoordinate();
 
+        return max.getLatitude() - min.getLatitude() * max.getLongitude() - min.getLongitude();
+    }
 
+    public double getFullElevation() {
+        double elevation = 0;
 
+        for (int i = 0; i < trackPoints.size() - 1; i++) {
+            TrackPoint actual = trackPoints.get(i);
+            TrackPoint next = trackPoints.get(i + 1);
+            if (actual.higherThan(next)) {                  //Trackpointban van ez a metódus
+                elevation += actual.elevationDifference(next); //Trackpointban
+            }
+        }
+        return elevation;
+    }
+
+           /*  foreach használatával
+        TrackPoint prev = trackPoints.get(0);
+        for (TrackPoint actual : trackPoints) {
+            if (actual.higherThan(prev)) {
+                elevation += actual.elevationDifference(prev);
+            }
+            prev = actual;
+        }
+        return elevation;
+     }
+    */
+
+    public double getFullDecrease() {
+        double decreaseSum = 0;
+        for (int i = 0; i < trackPoints.size() - 1; i++) {
+            TrackPoint actual = trackPoints.get(i);
+            TrackPoint next = trackPoints.get(i + 1);
+            if (next.higherThan(actual)) {                  //Trackpointban van ez a metódus
+                decreaseSum += actual.elevationDifference(next);
+            }
+        }
+        return decreaseSum;
+
+    }
 
     public List<TrackPoint> getTrackPoints() {
         return trackPoints;
     }
 
- //   public double getDistance() {
 
-  //  }
 }
+
+
