e6f20b577e5d1e39d191f3e325dd7d97aac0249f
Edina <terjek.edina@gmail.com>
2021-02-07 21:43:03 +0100

    Hány különböző betű van a szóban?

---
+++ src/main/java/week13d05/LetterCounter.java
@@ -0,0 +1,38 @@
+package week13d05;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LetterCounter {
+
+    public Map<String, Integer> countLetter(String word) {
+        Map<String, Integer> abc = new HashMap<>();
+
+        String[] letters = word.toLowerCase().split("");
+
+        for (String letter : letters) {
+            Integer prevValue = abc.get(letter);
+
+            if (!abc.containsKey(letter)) {
+                abc.put(letter, 1);
+            } else {
+                abc.put(letter, prevValue + 1);
+            }
+
+        }
+        return abc;
+    }
+
+
+    public static void main(String[] args) {
+       LetterCounter letterCounter = new LetterCounter();
+        System.out.println(letterCounter.countLetter("paprika"));
+
+    }
+
+}
+
+
+//Írj egy metódust, amely megszámolja, hogy hány különböző betű van egy szóban.
+// A kis és nagybetűk közötti különbség nem számít! A space-eket, számjegyeket, stb. ne vegye figyelembe!
+// Csak az angol ábécé betűit!
