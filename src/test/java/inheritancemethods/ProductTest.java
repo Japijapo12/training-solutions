104d8f997ab9c8332c71fc1be283ea72f18be496
Edina <terjek.edina@gmail.com>
2021-02-09 17:44:33 +0100

    Product és PackedProduct - metódusok öröklődése Teszt

---
+++ src/test/java/inheritancemethods/ProductTest.java
@@ -0,0 +1,48 @@
+package inheritancemethods;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ProductTest {
+
+    @Test
+    public void constructorTest3Parameters() {
+        //Given
+        BigDecimal unit = new BigDecimal("0.235");
+        Product product = new Product("Ipad", unit, 3);
+        //Then
+        assertEquals("Ipad", product.getName());
+        assertEquals(unit, product.getUnitWeight());
+        assertEquals(3, product.getNumberOfDecimals());
+    }
+
+    @Test
+    public void constructorTest2Parameters() {
+        //Given
+        BigDecimal unit = new BigDecimal("0.235");
+        Product product = new Product("Ipad", unit);
+        //Then
+        assertEquals("Ipad", product.getName());
+        assertEquals(unit, product.getUnitWeight());
+        assertEquals(2, product.getNumberOfDecimals());
+    }
+
+    @Test
+    public void totalWeight() {
+        //Given
+        BigDecimal unit = new BigDecimal("0.235");
+        Product product = new Product("Ipad", unit, 3);
+        BigDecimal total = new BigDecimal("0.705");
+        //Then
+        assertEquals(total, product.totalWeight(3));
+
+        //return unitWeight.multiply(new BigDecimal(String.valueOf(pieces))).setScale(numberOfDecimals, RoundingMode.HALF_UP);
+        //0.235 * 3 = 0.705
+    }
+}
