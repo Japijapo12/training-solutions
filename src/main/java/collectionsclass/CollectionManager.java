302741ba401562e386cbd63c8c61cabb13102f11
Edina <terjek.edina@gmail.com>
2021-02-13 14:25:13 +0100

    Könyvtár - módosíthatatlan, reverse ,min, max listák

---
+++ src/main/java/collectionsclass/CollectionManager.java
@@ -0,0 +1,35 @@
+package collectionsclass;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionManager {
+
+    List<Book> library = new ArrayList<>();
+
+    public CollectionManager(List<Book> library) {
+        this.library = library;
+    }
+
+    public List<Book> createUnmodifiableLibrary() {
+        return Collections.unmodifiableList(this.library);
+    }
+
+    public List<Book> reverseLibrary() {   // az eredeti lista másolatán dolgozik
+        List<Book> reverseList = new ArrayList<>(library);
+        Collections.sort(reverseList);
+        Collections.reverse(reverseList);
+
+        return reverseList;
+    }
+
+    public Book getFirstBook() {  //=legrégibb könyv -> legkisebb id
+        return Collections.min(library);
+    }
+
+    public Book getLastBook() {
+        return Collections.max(library);
+    }
+
+}
