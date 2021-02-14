cafc25f4e92eb74416a14fdabacbb9311ff0397d
Edina <terjek.edina@gmail.com>
2021-02-14 22:44:00 +0100

    Múltkori vizsga -ismétlés Páratlan számok tömbje

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -1,25 +1,35 @@
 package exam02;
 
+import java.util.Arrays;
+
 public class ArraySelector {
 
 
-    public String selectEvens(int[] events) {
+    public String selectEvens(int[] values) {
         String str = "";
-        if ( events == null) {
+        if ( values.length < 1) {
             return "";
         }
 
-        for ( int i = 0; i < events.length; i++) {
+        int[] newValues = new int[(int)Math.round(values.length / 2.0)];
 
-
-            if (i % 2 == 0) {
-              str =  "[" + str + ( (events[i] + ", ").toString()) + "]";
+        for ( int i = 0; i < values.length; i++) {
+           if (i % 2 == 0) {
+              newValues[i/2] = values[i];
             }
 
         }
-        return str;
+        return Arrays.toString(newValues);
     }
 
-
+    public static void main(String[] args) {
+        ArraySelector arraySelector = new ArraySelector();
+        System.out.println(arraySelector.selectEvens(new int[]{1,2,3,4,5}));
+    }
 
 }
+
+/*Írj egy olyan metódust az ArraySelector osztályban, mely egy kapott tömbből minden második elemet kiválogat, és egy szövegként összerak,
+szögletes zárójelek között!
+A páros megállapítása index szerint működik. Azaz az első, nulla indexű elem páros, a második, azaz egyes indexű elem páratlan, stb.
+Ha nulla elemű a tömb, akkor üres stringet adjon vissza!*/
