9f81fed79bcd60148f1adacde2cc313783aa9d19
Edina <terjek.edina@gmail.com>
2021-02-14 22:44:33 +0100

    Múltkori vizsga -ismétlés Életrajz Skills

--- src/main/java/exam02/cv/Cv.java
+++ src/main/java/exam02/cv/Cv.java
@@ -1,15 +1,15 @@
 package exam02.cv;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cv {
 
     private String name;
-    List<Skill> skills;
+    List<Skill> skills = new ArrayList<>();   //programming (5)
 
-    public Cv(String name, List<Skill> skills) {
+    public Cv(String name) {
         this.name = name;
-        this.skills = skills;
     }
 
     public String getName() {
@@ -20,8 +20,32 @@ public class Cv {
         return skills;
     }
 
-    public  void findSkillLevelByName() {
+    //programming (5)
+    public void addSkills(String... skillParts) {
+        for (String skillPart : skillParts) {
+            String name = skillPart.substring(0, skillPart.indexOf("(") - 1).trim();
+            int level = Integer.parseInt(skillPart.substring(skillPart.indexOf("(") + 1, skillPart.indexOf(")")));
+            Skill skill = new Skill(name, level);
+            skills.add(skill);
+        }
+    }
+
+
+    public int findSkillLevelByName(String name) {
+
+        for (Skill skill : skills) {
+            if (skill.getName().equals(name)) {
+                return skill.getLevel();
+            }
+        }
+        throw new SkillNotFoundException ("Skill not found: " + name);
 
     }
 
 }
+/*
+Egy önéletrajzot reprezentáló osztályt kell létrehozni, melyben van egy név, valamint skillek (hogy mihez ért az illető).
+A skill egy névvel és egy számmal (szint) rendelkezik, utóbbi 1 - 5 között.
+Az önéletrajz a Cv osztály pár metódussal, a skill a Skill osztály.
+A skillek megadása a nevével, valamint a szintjével történik, pl. programming (5).
+A findSkillLevelByName() a skill neve alapján visszaadja annak szintjét. */
