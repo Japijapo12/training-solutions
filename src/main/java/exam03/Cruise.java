66c076112744d71d59797c4b619cbeb8e091cdfa
Edina <terjek.edina@gmail.com>
2021-02-15 20:41:03 +0100

    exam03 - vizsgaidő

---
+++ src/main/java/exam03/Cruise.java
@@ -0,0 +1,48 @@
+package exam03;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cruise {
+
+    private Boat boat;
+    private LocalDate sailing;
+    private double basicPrice;
+    private List<Passenger> passengers = new ArrayList<>();
+
+    public Cruise(Boat boat, LocalDate sailing, double basePrice) {
+        this.boat = boat;
+        this.sailing = sailing;
+        this.basicPrice = basePrice;
+    }
+
+    public Boat getBoat() {
+        return boat;
+    }
+
+    public LocalDate getSailing() {
+        return sailing;
+    }
+
+    public double getBasicPrice() {
+        return basicPrice;
+    }
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
+
+
+
+
+    public void bookPassenger(Passenger passenger) {
+        if(passengers.size() > boat.getMaxPassengers()) {
+            throw new IllegalArgumentException("Nincs több hely a hajóra");
+        }
+        else {
+             passengers.add(passenger);
+            }
+        }
+    }
+
