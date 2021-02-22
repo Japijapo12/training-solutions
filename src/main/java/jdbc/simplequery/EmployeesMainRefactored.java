734f7f196a2c2e069e21ef6cb6a3355bc1a7fe73
Edina <terjek.edina@gmail.com>
2021-02-22 22:23:47 +0100

    Kapcsolódás adatbázishoz, select lekérdezés (Employee táblából)

---
+++ src/main/java/jdbc/simplequery/EmployeesMainRefactored.java
@@ -0,0 +1,56 @@
+package jdbc.simplequery;
+
+import org.mariadb.jdbc.MariaDbDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class EmployeesMainRefactored {
+    public static void main(String[] args) {
+        MariaDbDataSource dataSource;
+        try {
+            dataSource = new MariaDbDataSource();
+            dataSource.setUrl("jdbc:mariadb://localhost:3306/employees?useUnicode=true");
+            dataSource.setUser("employees");
+            dataSource.setPassword("employees");
+        } catch (
+                SQLException se) {
+            throw new IllegalStateException("Can not create data source", se);
+        }
+
+        new EmployeesMainRefactored().selectNameById(dataSource, 1L);
+
+    }
+
+//az első metódusban kiválasztjuk azt a nevet (emp_name), ahol  id=1, a második metódusban lekérdezzük.
+
+        public void selectNameById(DataSource dataSource, long id) {
+            try (Connection conn = dataSource.getConnection();
+                 PreparedStatement ps = conn.prepareStatement("select emp_name from employees where id = ?");
+            ) {
+                ps.setLong(1, id);
+                selectNameByPreparedStatement(ps);
+            }
+            catch (SQLException sqle) {
+                throw new IllegalStateException("Cannot query", sqle);
+            }
+        }
+
+    private void selectNameByPreparedStatement(PreparedStatement ps) {
+        try (ResultSet rs = ps.executeQuery()) {
+            if (rs.next()) {
+                String name = rs.getString("emp_name");
+                System.out.println(name);
+            }
+        } catch (SQLException sqle) {
+            throw new IllegalStateException("Cannot query", sqle);
+        }
+    }
+
+
+
+    }
+
