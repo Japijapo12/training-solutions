35a0ec8c89faee6c790ece2086a0b34de6f96583
Edina <terjek.edina@gmail.com>
2021-02-28 17:48:25 +0100

    Pótvizsga feladat - Levelezés

---
+++ src/main/java/exam03retake01/Contact.java
@@ -0,0 +1,33 @@
+package exam03retake01;
+
+public class Contact {
+
+    private String name;
+    private String email;
+
+    public Contact(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public boolean match(String s) {
+        return name.equals(s) || email.equals(s);
+    }
+}
