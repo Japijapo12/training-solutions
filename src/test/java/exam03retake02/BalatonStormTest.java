1aaf4bcba78759c294205c53101a9addc50cac4c
Edina <terjek.edina@gmail.com>
2021-03-06 19:35:19 +0100

    Balatoni vihar (2. pótvizsgafeladat) Teszt

---
+++ src/test/java/exam03retake02/BalatonStormTest.java
@@ -0,0 +1,23 @@
+package exam03retake02;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BalatonStormTest {
+
+    @Test
+    void testGetStationsInStorm() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(BalatonStormTest.class.getResourceAsStream("storm.json")))) {
+            assertEquals(List.of("Ábrahámhegy", "Balatonfüred", "Balatonszárszó"), new BalatonStorm().getStationsInStorm(reader));
+        }
+    }
+}
+
+//A fájl a az src/java/resources/ osztálykönyvtár mappában van!
+//de lehetne a test/resources könyvtárban is
