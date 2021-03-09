12d86b166386b6ba1a414967e3252e043f9b71ba
Edina <terjek.edina@gmail.com>
2021-03-10 00:09:40 +0100

    Covid project

---
+++ src/main/java/covid/Citizen.java
@@ -0,0 +1,61 @@
+package covid;
+
+import java.time.LocalDate;
+
+public class Citizen {
+    private String name;
+    private String zip;
+    private int age;
+    private String email;
+    private String taj;
+    private int numberOfVaccination;
+    private LocalDate lastVaccination;
+
+
+    public Citizen(String name, String zip, int age, String email, String taj, int numberOfVaccination, LocalDate lastVaccination) {
+        this.name = name;
+        this.zip = zip;
+        this.age = age;
+        this.email = email;
+        this.taj = taj;
+        this.numberOfVaccination = numberOfVaccination;
+        this.lastVaccination = lastVaccination;
+    }
+
+    public Citizen(String name, String zip, int age, String email, String taj) {
+        this.name = name;
+        this.zip = zip;
+        this.age = age;
+        this.email = email;
+        this.taj = taj;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTaj() {
+        return taj;
+    }
+
+    public int getNumberOfVaccination() {
+        return numberOfVaccination;
+    }
+
+    public LocalDate getLastVaccination() {
+        return lastVaccination;
+    }
+}
+
