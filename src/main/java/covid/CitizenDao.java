e5839eb5fc1855f6740137201342c5ad0488bafe
Edina <terjek.edina@gmail.com>
2021-03-13 00:09:00 +0100

    javítva

--- src/main/java/covid/CitizenDao.java
+++ src/main/java/covid/CitizenDao.java
@@ -4,7 +4,6 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CitizenDao {
@@ -32,37 +31,32 @@ public class CitizenDao {
         }
     }
 
-    public List<Citizen> insertCitizens(List<Citizen> citizens) {
-        if (citizens.isEmpty()) {
-            throw new IllegalArgumentException("Citizens list cannot be empty");
-        }
+    public void insertCitizens(List<Citizen> citizens) {
+
         try (Connection conn = dataSource.getConnection()) {
-            return insertCitizensByList(conn, citizens);
+
+            insertCitizensByList(conn, citizens);
+
         } catch (SQLException e) {
             throw new IllegalStateException("Cannot connect", e);
         }
     }
 
-        public List<Citizen> insertCitizensByList(Connection conn, List<Citizen> citizens) throws SQLException {
-            String sql = "Insert into `citizens`(`citizen_name`, `zip_code`, `age`, `email`, `taj`) values" + "(?,?,?,?,?),".repeat(citizens.size());
-            sql = sql.substring(0, sql.length() - 1);
-            List<Citizen> result = new ArrayList<>();
-
+        public void insertCitizensByList(Connection conn, List<Citizen> citizens) throws SQLException {
+            String sql = "Insert into `citizens`(`citizen_name`, `zip`, `age`, `email`, `taj`) values " + "(?,?,?,?,?)";
+            for (Citizen citizen : citizens) {
             try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                for (Citizen citizen : citizens) {
+
                     stmt.setString(1, citizen.getName());
                     stmt.setString(2, citizen.getZip());
                     stmt.setInt(3, citizen.getAge());
                     stmt.setString(4, citizen.getEmail());
                     stmt.setString(5, citizen.getTaj());
-                }
-
                     stmt.executeUpdate();
-
-                    return result;
-
-            }  catch (SQLException se) {
-                throw new IllegalStateException("Cannot insert", se);
+                }
+             catch(SQLException se){
+                    throw new IllegalStateException("Cannot insert", se);
+                }
             }
         }
 }
